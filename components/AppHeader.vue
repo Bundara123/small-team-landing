f53592f76f101e34f694aba83e44d9da3c441b1c
Bundara REUK <reuk.bundara@ababank.com>
2024-01-29 15:39:24 +0700

    init project

---
+++ components/AppHeader.vue
@@ -0,0 +1,5 @@
+<template>
+  <header class="bg-white backdrop-blur-md py-2 dark:bg-gray-900/80 sticky top-0">
+    Header
+  </header>
+</template>
---
+++ layouts/default.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="flex w-full flex-col">
+    <AppHeader />
+    <slot />
+    <AppFooter />
+  </div>
+</template>
+<style lang="scss">
+body {
+  padding: 0px;
+  margin: 0px;
+}
+</style>
